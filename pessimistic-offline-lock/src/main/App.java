9d25005a848612db72c026884e8513751633484a
Jin Hyun <SangJinHyun@tutanota.com>
2021-11-21 13:31:10 -0600

    redo file names

--- pessimistic-offline-lock/src/main/App.java
+++ pessimistic-offline-lock/src/main/App.java
@@ -1,16 +1,2 @@
-package com.iluwater.pessimisticfflinelock;
 
 
-@Slf4j
-public class App {
-
-    /**
-     * Program entry point.
-     *
-     * @param args command line args
-     */
-    public static void main(String[] args) {
-
-    }
-
-}

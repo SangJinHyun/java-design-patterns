656ac9c528c82389f3a0cb699cb85789ce732beb
Jin Hyun <SangJinHyun@tutanota.com>
2021-11-21 20:33:10 -0600

    update pessimistic

--- pessimistic-offline-lock/src/main/java/com/iluwatar/pessimisticlock/PessimisticLock.java
+++ pessimistic-offline-lock/src/main/java/com/iluwatar/pessimisticlock/PessimisticLock.java
@@ -1,8 +1,8 @@
 package com.iluwatar.pessimisticlock;
 
-
 import javax.persistence.Entity;
 import javax.persistenc.Id;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 
 import javax.persistence.Id;
@@ -21,13 +21,27 @@ public class PessimisticLock {
         //setup for service data
 
         // add waitime config
+    }
 
 
+    class Doc{
+
+        private long nameId;
+        private Integer num;
+
+    }
+
+    @Repository
+    interface DocRepo extends JpaRepository <Doc, Long>{
+
+        @Lock(LockModeType.PESSIMISTIC_WRITE)
+        @QueryHints({@QueryHint(name = "javax.persistence.lock.timeout", value = "3000")})
 
 
     }
 
 
 
-
 }
+
+

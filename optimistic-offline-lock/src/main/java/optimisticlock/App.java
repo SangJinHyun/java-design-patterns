765e6156d55512d6cb7cf897c0c32b91ab8190dd
Jin Hyun <SangJinHyun@tutanota.com>
2021-12-07 17:39:56 -0600

    update

--- optimistic-offline-lock/src/main/java/optimisticlock/App.java
+++ optimistic-offline-lock/src/main/java/optimisticlock/App.java
@@ -1,4 +1,12 @@
 package optimisticlock;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
 }
---
+++ optimistic-offline-lock/src/main/java/optimisticlock/User.java
@@ -0,0 +1,19 @@
+package optimisticlock;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Version;
+
+@Entity
+@Table( name = "person")
+public class User {
+    
+
+
+
+}
